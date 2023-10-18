8f054aa55889e03ba7d062ca2eb4ff0108ac863f
Justin <justin@Justins-MacBook-Pro.local>
2023-10-18 09:22:30 +0800

    finally listener is outputing something

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+//webpack.functions.js
+// const nodeExternals = require('webpack-node-externals');
+
+// module.exports = {
+//   externals: [nodeExternals()],
+// };
+
+module.exports = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.externals.push({
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'bufferutil': 'commonjs bufferutil',
+      })
+      return config
+    },
+}
+
