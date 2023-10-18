87148fc126d0af9a6a0355a174d1b2ccc729a32f
Justin <justin@Justins-MacBook-Pro.local>
2023-10-18 09:50:02 +0800

    yarn run build

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-// module.exports = nextConfig
-
-//webpack.functions.js
-// const nodeExternals = require('webpack-node-externals');
-
-// module.exports = {
-//   externals: [nodeExternals()],
-// };
 
 module.exports = {
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
